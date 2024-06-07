425832fd6db97592b94eb8be8ef615c26f103afd
BelzDaniel <BelzDaniel6@gmail.com>
2024-06-07 17:23:24 +0300

    first initial

---
+++ spider.js
@@ -0,0 +1,9 @@
+const request = require("request");
+const fs = require("fs");
+const mkdirp = require("mkdirp");
+const path = require("path");
+const utilities = require("./helpers/utilities.js");
+
+function spider() {
+  const filename = utilities.urlToFilename(url)
+}
